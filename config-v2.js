94081ab1fcea5b12d2c2e12b7f9755bb4ede62a9
ikhlaqahmedbugti3 <ikhlaqahmedbugti3@gmail.com>
2026-01-11 15:50:23 +0500

    Modify BOT_NUMBER and SESSION_ID in config-v2.js
    Updated WhatsApp bot number and session ID in config.

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
-const BOT_NUMBER = "923334104708" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
-const SESSION_ID = "" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
+const BOT_NUMBER = "923346864922" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
+const SESSION_ID = "PRABATH-MD~nzzFVOgs5css9Nj" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
 // ඉහත ඒව "" මැදට එකතු කරන්න වෙනත් කිසිම දෙයක් වෙනස් නොකරන්න ❌. (Add "" in the middle of the above and ❌ don't change anything else.)
